7accbca65e0cff73de721ce68c8a02bfb4d25901
Bilek-Petr <p.bilek29@gmail.com>
2024-11-18 11:23:36 +0100

    removed initial points, replaced with clickable

--- src/components/MapLayout/Map.tsx
+++ src/components/MapLayout/Map.tsx
@@ -6,18 +6,13 @@ import 'leaflet-routing-machine';
 import RoutingControl from './RoutingControl';
 
 const Map: React.FC = () => {
-   const waypoints: [number, number][] = [
-      [57.74, 11.94],
-      [57.6792, 11.949],
-   ];
-
    return (
-      <MapContainer center={[57.74, 11.94]} zoom={13} className="h-[100vh]">
+      <MapContainer center={[50.084, 15.007]} zoom={8} minZoom={3} className="h-[100vh]">
          <TileLayer
             attribution='<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
             url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
          />
-         <RoutingControl waypoints={waypoints} />
+         <RoutingControl />
       </MapContainer>
    );
 };
