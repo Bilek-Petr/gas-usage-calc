46df0b57f55bbeb91db21c0182f7e8127260742c
Bilek-Petr <p.bilek29@gmail.com>
2024-11-18 16:51:27 +0100

    linked styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import Map from "./components/MapLayout/Map";
-
+import Map from './components/MapLayout/Map';
+import './assets/styles/main.scss';
 
 export default function App() {
-  return (
-    <>
-          <Map></Map>
-    </>
-  )
+   return (
+      <>
+         <Map></Map>
+      </>
+   );
 }
-
